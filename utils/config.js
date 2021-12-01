5950b2453e293ff750dc18a9809e5d24172a5d86
gzaharija <gzaharija@pmfst.hr>
2021-12-01 09:02:10 +0100

    kraj predavanja

--- utils/config.js
+++ utils/config.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT
 // Baza podataka
 const password = process.env.ATLAS_PASS
 const user = process.env.ATLAS_USER
-const dbname = 'poruke-api'
+const dbname = proces.env.NODE_ENV === 'test' ? 'poruke-api-test' : 'poruke-api'
 const DB_URI = `mongodb+srv://${user}:${password}@cluster0.hc5nq.mongodb.net/${dbname}?retryWrites=true&w=majority`
 
 module.exports = {PORT, DB_URI}
