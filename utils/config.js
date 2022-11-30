65ccd4d33a3e46db49a39e176e2aa476805e370c
gzaharija <gzaharija@pmfst.hr>
2022-11-30 13:40:43 +0100

    2022

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 
 // Baza podataka
-const password = process.env.ATLAS_PASS
+const password = process.env.ATLAS_PASS 
 const user = process.env.ATLAS_USER
 const dbname = 'poruke-api'
 const DB_URI = `mongodb+srv://${user}:${password}@cluster0.l0kev.mongodb.net/${dbname}?retryWrites=true&w=majority`
